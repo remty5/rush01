39ea1d418abb31c77f4996898e8fec03602eb0fa
Alann Maricourt <almarico@c1r5p8.42lehavre.fr>
2023-07-15 17:22:43 +0200

    add some write

--- ex00/complete_tab.c
+++ ex00/complete_tab.c
@@ -6,7 +6,7 @@
 /*   By: almarico <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 15:09:30 by almarico          #+#    #+#             */
-/*   Updated: 2023/07/15 15:49:49 by rvandepu         ###   ########.fr       */
+/*   Updated: 2023/07/15 17:17:59 by almarico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,28 @@ int	**ft_fullfill_column(int **tab, int i, int value)
 	return (tab);
 }
 
+int	**ft_fullfill_line(int **tab, int i, int value)
+{
+	int	j;
+	int	number;
+
+	i %= 8;
+	j = 0;
+	if (value == 4)
+	{
+		number = 1;
+		while (j <= 4 && number <= 4)
+			tab[i][j++] = number++;
+	}
+	if (value == 1)
+	{
+		number = 4;
+		while (j <= 4 && number >= 1)
+			tab[i][j++] = number--;
+	}
+	return (tab);
+}
+
 int	**complete_tab(int **tab, int *arg)
 {
 	int	i;
@@ -42,6 +64,19 @@ int	**complete_tab(int **tab, int *arg)
 			ft_fullfill_column(tab, i, arg[i]);
 		if (arg[i] == 1 && arg[i + 4] == 4)
 			ft_fullfill_column(tab, i, arg[i]);
+		if (arg[i] == 1 && arg[i + 4] != 4)
+			tab[0][i] = 4;
+		i++;
+	}
+	i += 4;
+	while (i <= 11)
+	{
+		if (arg[i] == 4 && arg[i + 4] == 1)
+			ft_fullfill_line(tab, i, arg[i]);
+		if (arg[i] == 1 && arg[i + 4] == 4)
+			ft_fullfill_line(tab, i, arg[i]);
+		if (arg[i] == 1 && arg[i + 4] != 4)
+			tab[i][0] = 4;
 		i++;
 	}
 	return (tab);
