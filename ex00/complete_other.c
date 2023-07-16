6b097e77e2026c0f0fb204a7c57bb3e9bfec4a37
Alann Maricourt <almarico@c1r5p8.42lehavre.fr>
2023-07-16 12:10:26 +0200

    try something

--- ex00/complete_other.c
+++ ex00/complete_other.c
@@ -6,32 +6,21 @@
 /*   By: almarico <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 18:19:58 by almarico          #+#    #+#             */
-/*   Updated: 2023/07/15 22:35:41 by almarico         ###   ########.fr       */
+/*   Updated: 2023/07/16 12:08:28 by almarico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	**ft_complete_comb(int **tab, int value, int arg_index)
 {
-	int	i;
-	int	number;
 
-	i = 0;
-	number = 1;
-	if (arg_index <= 3)
-	{
-		while (value >= 1)
-			tab[i++][arg_index] = 5 - value--;
-		while (i <= 3)
-			tab[i++][arg_index] = number++;
-	}
-	if (arg_index <= 11)
-	{
-		arg_index %= 4;
-		while (value >= 1)
-			tab[arg_index][i++] = 5 - value--;
-		while (i <= 3)
-			tab[arg_index][i++] = number++;
-	}
+	if (arg_index <= 3 && value == 3)
+		tab[value - 1][arg_index] = 4;
+	if (arg_index <= 7 && value == 3)
+		tab[value - 1][arg_index % 4] = 4;
+	if (arg_index <= 11 && value == 3)
+		tab[value - 1][arg_index % 4] = 4;
+	if (arg_index <= 15 && value == 3)
+		tab[value - 1][arg_index % 4] = 4;
 	return (tab);
 }
 
